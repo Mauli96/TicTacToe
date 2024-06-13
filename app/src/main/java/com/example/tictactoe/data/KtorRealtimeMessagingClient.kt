8bdf8b53c7f52d5ecde4bd8b95f9b49f85352a04
Mauli Waghmore <mauliwaghmore9696@gmail.com>
2024-06-13 22:59:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tictactoe/data/KtorRealtimeMessagingClient.kt
@@ -0,0 +1,45 @@
+package com.example.tictactoe.data
+
+import io.ktor.client.*
+import io.ktor.client.plugins.websocket.webSocketSession
+import io.ktor.client.request.*
+import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.close
+import io.ktor.websocket.readText
+import kotlinx.coroutines.flow.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class KtorRealtimeMessagingClient(
+    private val client: HttpClient
+): RealtimeMessagingClient {
+
+    private var session: WebSocketSession? = null
+
+    override fun getGameStateStream(): Flow<GameState> {
+        return flow {
+            session = client.webSocketSession {
+                url("ws://192.168.0.209:8081/play")
+            }
+            val gameStates = session!!
+                .incoming
+                .consumeAsFlow()
+                .filterIsInstance<Frame.Text>()
+                .mapNotNull { Json.decodeFromString<GameState>(it.readText()) }
+
+            emitAll(gameStates)
+        }
+    }
+
+    override suspend fun sendAction(action: MakeTurn) {
+        session?.outgoing?.send(
+            Frame.Text("make_turn#${Json.encodeToString(action)}")
+        )
+    }
+
+    override suspend fun close() {
+        session?.close()
+        session = null
+    }
+}
